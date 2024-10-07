c211cd96736e52f8d79b65746677c88d3ffe334b
Kamal Pratap <kamalpratapsingh288@gmail.com>
2024-10-08 00:21:16 +0530

    commit2

---
+++ 01variable.js
@@ -0,0 +1,17 @@
+const accountId = 164432
+let accountEmail = "kamalpratapsingh288@gmail.com"
+var accountpassword = "12334"
+ let accountcity = "orai"
+let accountstate;
+// aacoountId = 2//not allowed
+ 
+    
+accountpassword = "7689384"
+accountcity = "delhi"
+ 
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate ])
---
+++ 02datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all js as newer version
+
+//alert(3+3)
+ console.log(3+3)
+console.log("kamal")
+
+let name = "kaamalpratap"
+let age = "18"
+let isLoggedIn = false
+
+//number => 2 to power 45
+// bigint
+//string =>""
+//boolean => true/false
+//null  => standalone value 
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
