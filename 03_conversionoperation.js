8a47d859207817660db459a79ccefe54e930fc8a
Kamal Pratap <kamalpratapsingh288@gmail.com>
2024-10-10 00:39:37 +0530

    conversion of datatypes

---
+++ 03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score = "44"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"44abc"=>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+
+console.log(booleanIsLoggedIn);
+// 1 => true;
+// "" => false
+//"hitesh" = true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
